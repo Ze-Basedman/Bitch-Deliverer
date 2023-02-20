5170f4b36454dcd2f59f5869fe0263ae268e150f
ze <115031715+Ze-Basedman@users.noreply.github.com>
2023-02-20 10:41:50 +0200

    Add files via upload

---
+++ bitchesget.cs
@@ -0,0 +1,11 @@
+public class trying_to_Get_bitches
+{
+    public static void Main(string[] args)
+    {
+        int bitches;
+        Console.Write("How many bitches do u want:");
+        int bitches = Console.ReadLine();
+        Console.Write("Getting the bitches...");
+        Console.Write("Your ", bitches, "Bitches will arrive soon");
+    }
+}
